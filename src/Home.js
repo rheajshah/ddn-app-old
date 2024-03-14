2e6b0da5e92f6a63a8039ce1a3bc14a503d3b78e
rheajshah <rheajshah@gmail.com>
2024-03-13 17:28:31 -0700

    home updates

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import "./Home.css";
 function Home() {
     return (
       <div className="home">
-        <div className="mt-3 bubble-light">
+        <div className="btn-container rounded-pill mt-3 bubble-light">
             <Link to="/comps" className="btn btn-primary rounded-pill ms-3 button-style home-button"> Comps </Link>
             <Link to="/elo" className="btn btn-primary rounded-pill ms-3 button-style home-button"> ELO </Link>
             <Link to="/teams" className="btn btn-primary rounded-pill ms-3 button-style home-button"> Teams </Link>
